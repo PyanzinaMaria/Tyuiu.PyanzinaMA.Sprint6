899e492e503cee51915e62818c7ae54c67327d89
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-12-11 03:35:54 +0500

    Task6

--- Tyuiu.PyanzinaMA.Sprint6.Task6.V20/FormMain.Designer.cs
+++ Tyuiu.PyanzinaMA.Sprint6.Task6.V20/FormMain.Designer.cs
@@ -29,10 +29,17 @@ namespace Tyuiu.PyanzinaMA.Sprint6.Task6.V20
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormMain";
+            this.Text = "Спринт 6 | Таск 6 | Вариант 20 | Пьянзина М.А.";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
